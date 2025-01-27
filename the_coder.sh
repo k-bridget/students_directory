32346418c4506e598b75bbceaa65c7207a4451ed
k-bridget <k.bridget@alustudent.com>
2025-01-27 12:18:15 +0200

    files updated

---
+++ the_coder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#The following line is for creating a directory called "students_directory"
+mkdir students_directory
+# In the directory above, we are adding three text files using the following command
+touch students_directory/cohort-1.txt
+touch students_diretory/cohort-2.txt
+touch students_directory/cohort-3.txt
